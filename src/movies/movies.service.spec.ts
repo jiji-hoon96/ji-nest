1598b6063f08a002012ad33cd5bdf3473c57ca12
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-11 19:38:10 +0900

    8 uni test 완료

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -42,4 +42,55 @@ describe('MoviesService', () => {
       }
     })
   })
+  describe('deleteOne',()=>{
+    it("deletes a movie", ()=>{
+      service.create({
+        title:"테스트를 또해보자",
+        year: 20222,
+        genres:["테",'성'],
+      })
+      const beforeDelete= service.getAll().length;
+      service.deleteOne(1)
+      const afterDelete= service.getAll().length;
+      expect(afterDelete).toBeLessThan(beforeDelete);
+    })
+    it("should throw 404 error", ()=>{ //  존재하지 않는 movieId에 에러가 생성되는지
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+      })
+  })
+  describe('create',()=>{
+    it("should create a movie",()=>{
+      const beforeCreate = service.getAll().length;
+      service.create({
+        title:"테스트를 또해보자",
+        year: 20222,
+        genres:["테",'성'],
+      })
+      const afterCreate = service.getAll().length;
+      expect(afterCreate).toBeGreaterThan(beforeCreate)
+    })
+  })
+  describe("update",()=>{
+    it("should update a movie", ()=>{
+      service.create({
+        title:"테스트를 또해보자",
+        year: 20222,
+        genres:["테",'성'],
+      });
+      service.update(1, {title :"바꾸자~"});
+      const movie = service.getOne(1);
+      expect(movie.title).toEqual('바꾸자~');
+    })
+    it("should throw a NotFoundExe", ()=>{ //  존재하지 않는 movieId에 에러가 생성되는지
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+      }
+      })
+  })
 });
