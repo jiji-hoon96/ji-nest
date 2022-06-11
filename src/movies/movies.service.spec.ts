48e10be4eb770492355ab70e09942ce2b57a24cf
jiji-hoon96 <94469974+jiji-hoon96@users.noreply.github.com>
2022-06-11 19:04:53 +0900

    7. expect를 이용해 test

--- src/movies/movies.service.spec.ts
+++ src/movies/movies.service.spec.ts
@@ -1,3 +1,4 @@
+import { NotFoundException } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import { MoviesService } from './movies.service';
 
@@ -15,4 +16,30 @@ describe('MoviesService', () => {
   it('should be defined', () => {
     expect(service).toBeDefined();
   });
+  describe("getAll",()=>{
+    it('should return an array',()=>{
+      const result= service.getAll();
+      expect(result).toBeInstanceOf(Array); //result(getAll)가 array인지 테스트
+    })
+  })
+  describe("getOne",()=>{
+    it("should return a movie", ()=>{
+      service.create({
+        title: "테스트용",
+        year: 2022,
+        genres : ["테스트","성공적"]
+      })
+      const movie= service.getOne(1)
+      expect(movie).toBeDefined(); // 위에서 만든 movie가 존재하는지
+      expect(movie.id).toEqual(1); // 위에서 만든 movie의 아이디가 일치하는지
+    });
+    it("should throw 404 error", ()=>{ //  존재하지 않는 movieId에 에러가 생성되는지
+      try{
+        service.getOne(999);
+      }catch(e){
+        expect(e).toBeInstanceOf(NotFoundException);
+        expect(e.message).toEqual("해당 999의 영화는 존재하지 않습니다");
+      }
+    })
+  })
 });
